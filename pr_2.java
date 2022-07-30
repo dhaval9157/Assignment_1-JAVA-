880a30e500ecf794ccd5081f062398ac22b89877
dhaval9157 <109013208+dhaval9157@users.noreply.github.com>
2022-07-30 10:24:39 +0530

    Add files via upload

---
+++ pr_2.java
@@ -0,0 +1,36 @@
+// //Given a string, return a string made of the first
+// 2 chars (if present), however include
+// first char only if it is 'o' and include the second
+// only if it is 'z', so "ozymandias"
+// yields "oz".
+// startOz("ozymandias") → "oz
+// startOz("bzoo") → "z"
+// startOz("oxx") → "o"
+import java.util.*;
+public class pr_2 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the String : ");
+        String s = sc.nextLine();
+
+        String answer = startOz(s);
+        System.out.println(answer);
+        sc.close();
+        System.out.println("Prepared by Dhaval_21CE089");
+    }
+
+    public static String startOz(String a)
+    {
+        String ans = "";
+        if(a.charAt(0)=='o')
+            ans=ans+'o';
+
+        if(a.charAt(1)=='z')
+            ans=ans+'z';
+
+        return ans;
+    }
+
+}
